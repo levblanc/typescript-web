bd60ec67feaff81c8c1a7cfed8eef37569d053a9
levblanc <levblanc@gmail.com>
2022-08-10 11:11:50 +0800

    Implement first version of `User` class

---
+++ src/models/User.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosResponse } from 'axios';
+
+interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  // `[key: string]` tells TS
+  // we are going to set a key to the object
+  // its type is going to be string
+  // but we don't know its name yet
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
+}
