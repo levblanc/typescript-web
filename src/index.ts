780dc753953b1f6c749d4dc244a4bd47aa61634d
levblanc <levblanc@gmail.com>
2022-08-10 17:22:51 +0800

    Wire all methods into `User` class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 0 });
+const user = new User({ id: 1, name: 'newer name', age: 999 });
+// console.log(user.get('name'));
 
-user.sync.save();
+// `on` is on a getter
+// so the parentheses are triggering
+// the `on` function inside `Eventing` class
+// NOT the one inside `User` class
+user.on('save', () => {
+  console.log(user);
+});
 
-
+user.save()
