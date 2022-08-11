70d9a73e64ecb9f3b7b8e3f7f2f281e5dc6c1adb
levblanc <levblanc@gmail.com>
2022-08-11 10:32:24 +0800

    Refactor with inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer name', age: 999 });
+const user = User.buildUser({ id: 1});
 // console.log(user.get('name'));
 
 // `on` is on a getter
 // so the parentheses are triggering
 // the `on` function inside `Eventing` class
 // NOT the one inside `User` class
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 });
 
-user.save();
+user.fetch();
